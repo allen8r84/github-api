b2a7fa121fca3fd6b8e72af31561aa3b1fe65782
Jon <jon@allen8r.com>
2015-02-28 12:56:51 -0700

    finish project

--- controller.js
+++ controller.js
@@ -1,5 +1,12 @@
 var app = angular.module('github-api');
 
-app.controller('MainController', function($scope){
+app.controller('GitHubController', function($scope, githubService){
+    $scope.getUserData = function(){
+        githubService.getUser($scope.uesrText).then(function(res){
+            $scope.user = res.data;
+            console.log($scope.user);
+        });
+    }
+    
     
 });
--- service.js
+++ service.js
@@ -1,7 +1,14 @@
 var app = angular.module('github-api');
 
-app.service('mainService', function($http, $q) {
-  var id = "myCLientID";
-  var sec = "mySecretKeyThing";
-  var param = "?client_id=" + id + "&client_secret=" + sec;
+
+app.service('githubService', function($http) {
+    var id = "3257a93c96b37cf80ae9";
+    var sec = "45becb1dda65f5af435cb81048b8791bd02440f9﻿";
+    var param = "?client_id=" + id + "&client_secret=" + sec;  
+  this.getUser = function(userName){
+        return $http({
+            method: 'GET',
+            url: 'https://api.github.com/users/' + userName + param
+        });
+    }
 });
